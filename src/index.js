381ebfff942fc35233dafdfd6e2bf694f09aa9a6
Abigail Chelanagt <abigailchelah@gmail.com>
2025-04-28 09:42:43 +0300

    Add my files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.querySelector("input#searchByID");
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const movieDetails = document.querySelector("#movieDetails");
+          movieDetails.innerHTML = `
+            <h4>${data.title}</h4>
+            <p>${data.summary}</p>
+          `;
+        })
+        .catch((error) => {
+          console.error("Error fetching movie:", error);
+        });
+    });
+  };
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
